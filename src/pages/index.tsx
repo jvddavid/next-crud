5e988ebf53494ac327d74498f29577bac9dd367a
João Victor Davi <j.victordavid2@gmail.com>
2022-04-28 15:17:17 -0300

    style: alter background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center">
+    <div className="flex h-screen justify-center items-center bg-gradient-to-tr from-purple-500 to-blue-400">
       <span className="text-4xl">Texto</span>
     </div>
   )
