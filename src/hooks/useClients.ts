45a7cea85ac15c7cfb1a1c32ca5aeb08ff596d3d
João Victor Davi <j.victordavid2@gmail.com>
2022-04-30 13:18:57 -0300

    reafactor: remove useRef and move Pagination to other hook

--- src/hooks/useClients.ts
+++ src/hooks/useClients.ts
@@ -3,44 +3,44 @@ Author: João Victor David de Oliveira (j.victordavid2@gmail.com)
 useClients.ts (c) 2022
 Desc: description
 Created:  2022-04-30T16:02:37.615Z
-Modified: 2022-04-30T16:04:50.079Z
+Modified: 2022-04-30T16:17:17.213Z
 */
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import CollectionClient from "../backend/db/collectionClient";
 import Client from "../core/Client";
-import ClientRepo from "../core/ClientRepo";
+import usePage from "./usePage";
 
 export default function useClients() {
-  const repo = useRef<ClientRepo>(new CollectionClient())
+  const { page, setPage } = usePage();
+
+  const repo = new CollectionClient()
 
   const [client, setClient] = useState(Client.vazio());
-  const [page, setPage] = useState(0);
   const [clients, setClients] = useState([]);
 
-  useEffect(() => {
-    getClients()
-  }, [repo])
+  useEffect(() => void getClients())
 
   async function getClients() {
-    await repo.current.getAll().then(setClients);
-    setPage(0)
+    await repo.getAll().then(setClients);
   }
 
   async function onClientDeleted(client: Client) {
     if (client.id) {
-      await repo.current.delete(client);
+      await repo.delete(client);
     }
     await getClients();
+    setPage(0)
   }
 
   async function onClientUpdated(client: Client) {
     if (client.id) {
-      await repo.current.save(client);
+      await repo.save(client);
     } else {
-      await repo.current.save(client);
+      await repo.save(client);
     }
     await getClients();
+    setPage(0)
   }
 
   function onNewClient() {
@@ -54,9 +54,9 @@ export default function useClients() {
   }
 
   return {
-    client,
     page,
     setPage,
+    client,
     clients,
     onClientDeleted,
     onClientUpdated,
