0cb74f38c0142d12b4c355c87f0ab093b6d81c37
João Victor Davi <j.victordavid2@gmail.com>
2022-04-30 00:16:02 -0300

    refactor: modify bg of hr

--- src/components/Titulo.tsx
+++ src/components/Titulo.tsx
@@ -3,7 +3,7 @@ Author: João Victor David de Oliveira (j.victordavid2@gmail.com)
 Titulo.tsx (c) 2022
 Desc: description
 Created:  2022-04-30T01:01:45.113Z
-Modified: 2022-04-30T01:32:09.054Z
+Modified: 2022-04-30T02:05:55.725Z
 */
 
 export default function Titulo(props) {
@@ -12,7 +12,7 @@ export default function Titulo(props) {
       <h1 className="px-5 py-2 text-2xl">
         {props.children}
       </h1>
-      <hr className="border-2 border-purple-500" />
+      <hr className="border-2 bg-purple-500 border-purple-500" />
     </div>
   )
 }
