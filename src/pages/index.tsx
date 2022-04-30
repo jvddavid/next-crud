5893ff7ded76ec31f9107795839cc2752b904ee5
João Victor Davi <j.victordavid2@gmail.com>
2022-04-29 22:33:14 -0300

    chore: modify .gitignore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <div className="flex h-screen justify-center items-center bg-gradient-to-tr from-purple-500 to-blue-400">
-      <span className="text-4xl">Texto</span>
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
+      <Layout titulo="Cadastro Simples">
+        <span>Conteúdo</span>
+      </Layout>
     </div>
   )
 }
