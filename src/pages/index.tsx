2a512894d1039284a070e73b6d3d0d030025775b
João Victor Davi <j.victordavid2@gmail.com>
2022-04-30 02:48:04 -0300

    feat: create navigation and implement functions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,49 @@
+import { useState } from "react";
+import Button from "../components/Button";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
+import Client from "../core/Client";
+import Tabela from './../components/Tabela';
 
 export default function Home() {
+  const [client, setClient] = useState(Client.vazio());
+  const [page, setPage] = useState(0);
+  const [clients, setClients] = useState([
+    new Client('João', 10, '1'),
+    new Client('Maria', 20, '2'),
+    new Client('Pedro', 30, '3'),
+    new Client('José', 40, '4'),
+    new Client('Paulo', 50, '5'),
+  ]);
+
+  function onClientDeleted(client: Client) {
+    setClients(clients.filter(c => c.id !== client.id))
+    setPage(0)
+  }
+
+  function onClientSelected(client: Client) {
+    setClient(client)
+    setPage(1)
+  }
+
+  function onClientUpdated(client: Client) {
+    if (client.id) {
+      setClients(clients.map(c => c.id === client.id ? client : c))
+    } else {
+      setClients([...clients, new Client(client.nome, client.idade, (Math.random() * 1000).toFixed(0))])
+    }
+    setPage(0)
+  }
+
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
       <Layout titulo="Cadastro Simples">
-        <span>Conteúdo</span>
+        <div className="flex justify-end">
+          {page === 0 && <Button cor="green" onClick={() => { setClient(Client.vazio()); setPage(1) }}>Novo Cliente</Button>}
+          {page === 1 && <Button cor="gray" onClick={() => setPage(0)}>Voltar</Button>}
+        </div>
+        {page === 0 && <Tabela clients={clients} onClientDeleted={onClientDeleted} onClientSelected={onClientSelected} />}
+        {page === 1 && <Formulario client={client} onClientDeleted={onClientDeleted} onClientUpdated={onClientUpdated} />}
       </Layout>
     </div>
   )
